2b98f21889a898996213c083b5da5e8c95bf2189
furlat <tfurlanello@gmail.com>
2024-03-03 16:10:50 +0100

    fixed install to editable and added statements

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
  A Collection of Pydantic Models to Abstract language processing tasks and the realiaties analyzed therein. 
 
 
-python -m pip install pathto/Abstractions
+python -m pip install -e pathto/Abstractions
--- abstractions/goap/entity.py
+++ abstractions/goap/entity.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field, ValidationError, validator, field_validator
+from pydantic import BaseModel, Field, ValidationError, validator, field_validator, ValidationInfo
 from typing import Annotated, Any, Dict, List, Optional, Set, Union
 from pydantic.functional_validators import AfterValidator
 import uuid
@@ -43,6 +43,11 @@ class Attribute(BaseModel, RegistryHolder):
             self.name = self.__class__.__name__
         self.register(self)
 
+
+
+    
+
+
 class Entity(BaseModel, RegistryHolder):
     name: str = Field("", description="The name of the entity")
     id: str = Field(default_factory=lambda: str(uuid.uuid4()), description="The unique identifier of the entity")
@@ -52,7 +57,15 @@ class Entity(BaseModel, RegistryHolder):
         if not self.name:
             self.name = self.__class__.__name__
         self.register(self)
+        
+    
+    @field_validator('*', mode='after')
+    def check_attributes_and_entities(cls, v: Any, info: ValidationInfo):
+        if info.field_name not in ['id', 'name'] and not isinstance(v, (Attribute, Entity)):
+            raise ValueError(f"Attributes must be instances of Attribute or Entity, got {type(v).__name__} for field {info.field_name}")
+        return v
 
+    
     def all_attributes(self) -> Dict[str, 'Attribute']:
         attributes = {}
         for attribute_name, attribute_value in self.__dict__.items():
@@ -61,4 +74,27 @@ class Entity(BaseModel, RegistryHolder):
             elif isinstance(attribute_value, Entity):
                 nested_attributes = attribute_value.all_attributes()
                 attributes.update(nested_attributes)
+        return attributes
+    
+class Statement(BaseModel, RegistryHolder):
+    name: str = Field("", description="The name of the statement")
+    id: str = Field(default_factory=lambda: str(uuid.uuid4()), description="The unique identifier of the entity")
+
+    def __init__(self, **data: Any):
+        super().__init__(**data)
+        if not self.name:
+            self.name = self.__class__.__name__
+        self.register(self)
+    
+    @classmethod
+    def from_entity(cls, entity: Entity, name: Optional[str] = None):
+        """ initializes a statement using the flat attributes of an entity"""
+        attributes = entity.all_attributes()
+        return cls(name=name, **attributes)
+
+    def all_attributes(self) -> Dict[str, 'Attribute']:
+        attributes = {}
+        for attribute_name, attribute_value in self.__dict__.items():
+            if isinstance(attribute_value, Attribute):
+                attributes[attribute_name] = attribute_value
         return attributes
