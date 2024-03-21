5a4fd0e02bb40578010b78bca3debed525815aca
Cynde <furlanel@usc.edu>
2024-03-21 18:49:45 +0100

    Update viz.py

--- abstractions/utils/viz.py
+++ abstractions/utils/viz.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel
 def safe_schema_draw(schema: BaseModel, out: str = 'schema.png'):
     try:
         import erdantic as erd
-        erd.draw(at.Conversation, out = 'gang.png')
+        erd.draw(schema, out = 'gang.png')
     except ImportError as e:
         print(e)
         print("erdantic not installed, good luck with pygraphviz and graphviz installations!")
